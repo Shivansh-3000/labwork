b741b5a6b4e04116e37be44dc616d8f71d917466
Shivansh-3000 <78915727+Shivansh-3000@users.noreply.github.com>
2021-09-29 09:33:36 +0530

    Create armstrong.cpp

---
+++ armstrong.cpp
@@ -0,0 +1,32 @@
+//Name-Shivansh Gairola
+//Student Id - 20011506
+//Write a program to input a number from the user and check whether it is an armstrong number or not
+
+#include<iostream>
+#include<cstdlib>
+#include<math.h>
+using namespace std;
+
+int main()
+{
+int n,n1,c=0;
+cin>>n;
+n1=n;
+while(n1)
+{
+c++;
+n1 = n1/10;
+}
+n1=n;
+int sum=0;
+while(n1)
+{
+sum = sum + pow(n1%10,c);
+n1/=10;
+}
+if(n==sum)
+cout<<"It is an armstrong number";
+else
+cout<<"It is not a armstrong number";
+return 0;
+}
